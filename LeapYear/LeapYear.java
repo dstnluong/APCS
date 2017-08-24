00f48ceef42e4080e7c2df5ade9cef43222d15ec
dstnluong <dstn.luong@gmail.com>
2017-08-23 18:50:02 -0700

    Add files via upload

--- LeapYear/LeapYear.java
+++ LeapYear/LeapYear.java
@@ -1,1 +1,31 @@
-//
+//Dustin Luong (08/22/17)
+//Determine if a year is a leap year
+import java.util.Scanner;
+public class LeapYear {
+  public static void main(String [] args) {
+    System.out.println("This program tells you if a year is a leap year.");
+    System.out.println("Enter a year:");
+    Scanner in = new Scanner(System.in); //prompts user for a year
+    int year = in.nextInt();
+    String leapYear = year + " is a leap year.\n";
+    String notLeapYear = year + " is not a leap year.\n";
+
+    if (year < 0) {
+      System.out.print("Please enter a valid year.\n");
+    } else {
+        if (year % 4 == 0) { //checks that it is mod 4
+          if (year > 1582) { //bypasses mod 100 rule if year is less than 1582
+            if (year % 100 == 0 && year % 400 != 0) { //mod 400 is an exception to the mod 100 rule
+              System.out.print(notLeapYear);
+            } else {
+              System.out.print(leapYear);
+            }
+          } else {
+            System.out.print(leapYear);
+          }
+        } else {
+          System.out.print(notLeapYear);
+        }
+    }
+  }
+}
---
+++ LeapYear/README.md
@@ -0,0 +1,5 @@
+# LeapYear
+
+First APCS assignment of the year.
+
+A year is a leap year if it is divisible by 4 (1980), except that it is not a leap year if it is divisible by 100 (1900); however, it is a leap year if it is divisible by 400. There were no exceptions before the introduction of the Gregorian calendar on October 15, 1582 (for example, 1500 was a leap year). Write a program with a given year, determine if that year is a leap year or not.
