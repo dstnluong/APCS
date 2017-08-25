2a0e079dc3798809a7b3daa77b7713fb6c93ab28
Dustin Luong <dstn.luong@gmail.com>
2017-08-24 20:26:21 -0700

    added a rerun option

--- TimeInterval/TimeInterval.java
+++ TimeInterval/TimeInterval.java
@@ -3,41 +3,56 @@ import java.util.Scanner;
 public class TimeInterval{
     public static void main(String [] args){
 
-        //prompts user for first time
-        System.out.println("Enter the first time in military time:");
-        Scanner in = new Scanner(System.in);
-        int timeOne = in.nextInt();
+        boolean stay = true;
+        while (stay == true) {
 
-        //prompts user for second time
-        System.out.println("Enter the second time in military time:");
-        Scanner in2 =  new Scanner(System.in);
-        int timeTwo = in2.nextInt();
+            //prompts user for first time
+            System.out.println("Enter the first time in military time:");
+            Scanner in = new Scanner(System.in);
+            int timeOne = in.nextInt();
 
-        //check if the values entered are valid
-        if (timeOne > 2400 || timeTwo > 2400 || timeOne < 0 || timeTwo < 0 ||
-            timeOne % 100 >= 60 || timeTwo % 100 >= 60) {
-                System.out.println("Please enter valid times.");
-        } else {
+            //prompts user for second time
+            System.out.println("Enter the second time in military time:");
+            Scanner in2 =  new Scanner(System.in);
+            int timeTwo = in2.nextInt();
 
-            //convert time into minutes
-            int minutesOne = timeOne % 100;
-            int minutesTwo = timeTwo % 100;
-            int hourOne = timeOne / 100;
-            int hourTwo = timeTwo / 100;
-            int totalminutesOne = 60 * hourOne + minutesOne;
-            int totalminutesTwo = 60 * hourTwo + minutesTwo;
+            //check if the values entered are valid
+            if (timeOne > 2400 || timeTwo > 2400 || timeOne < 0 || timeTwo < 0 ||
+                timeOne % 100 >= 60 || timeTwo % 100 >= 60) {
+                    System.out.println("Please enter valid times.");
 
-            //find difference between the two times and reconvert into hours and minutes
-            int timeDiff = (totalminutesTwo - totalminutesOne + 1440) % 1440;
-            int timeDiffminutes = timeDiff % 60;
-            int timeDiffhours = timeDiff / 60;
+                    //edge case
+                } else if (timeOne % 2400 == timeTwo % 2400) {
+                    System.out.println("24 hours and 0 minutes");
+                } else {
 
-            //print the result
-            System.out.println(timeDiffhours + " " +
-                (timeDiffhours == 1 ? "hour" : "hours")
-                    + " and " + timeDiffminutes + " " +
-                (timeDiffminutes == 1 ? "minute" : "minutes"));
+                    //convert time into minutes
+                    int minutesOne = timeOne % 100;
+                    int minutesTwo = timeTwo % 100;
+                    int hourOne = timeOne / 100;
+                    int hourTwo = timeTwo / 100;
+                    int totalminutesOne = 60 * hourOne + minutesOne;
+                    int totalminutesTwo = 60 * hourTwo + minutesTwo;
+
+                    //find difference between the two times and reconvert into hours and minutes
+                    int timeDiff = (totalminutesTwo - totalminutesOne + 1440) % 1440;
+                    int timeDiffminutes = timeDiff % 60;
+                    int timeDiffhours = timeDiff / 60;
+
+                    //print the result
+                    System.out.println(timeDiffhours + " " +
+                        (timeDiffhours == 1 ? "hour" : "hours")
+                            + " and " + timeDiffminutes + " " +
+                        (timeDiffminutes == 1 ? "minute" : "minutes"));
+                 }
+            //rerun program TODO fix logic
+            System.out.println("Enter '1' to rerun. Enter '0' to end.");
+            Scanner input = new Scanner(System.in);
+            if (input.nextInt() == 1){
+                stay = true;
+            } else if (input.nextInt() == 0) {
+                stay = false;
+            }
         }
     }
-
 }
