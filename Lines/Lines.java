614147b3438120fbd172ec453ad525f9f013517a
Dustin Luong <dstn.luong@gmail.com>
2017-08-29 21:15:41 -0700

    Updated

--- Lines/Lines.java
+++ Lines/Lines.java
@@ -4,8 +4,8 @@ public class Lines {
         Scanner in = new Scanner(System.in);
         while(true){
             double m1 = 0;
-            double yInt1 = 0;
             double m2 = 0;
+            double yInt1 = 0;
             double yInt2 = 0;
             double vert1 = 0;
             double vert2 = 0;
@@ -17,31 +17,31 @@ public class Lines {
             System.out.println("4) vertical line");
             int line1 = in.nextInt();
             if(line1 == 1) {
-                 System.out.print("slope= ");
+                 System.out.print("slope=");
                  m1 = in.nextDouble();
-                 System.out.print("x-coordinate= ");
+                 System.out.print("x-coordinate=");
                  double x = in.nextDouble();
-                 System.out.print("y-coordinate= ");
+                 System.out.print("y-coordinate=");
                  double y = in.nextDouble();
                  yInt1 = y - m1 * x;
             } else if (line1 == 2){
-                System.out.print("x-coordinate of point 1= ");
+                System.out.print("x-coordinate of point 1=");
                 double x1 = in.nextDouble();
-                System.out.print("y-coordinate of point 2= ");
+                System.out.print("y-coordinate of point 2=");
                 double y1 = in.nextDouble();
-                System.out.print("x-coordinate= ");
+                System.out.print("x-coordinate=");
                 double x2 = in.nextDouble();
-                System.out.print("y-coordinate= ");
+                System.out.print("y-coordinate=");
                 double y2 = in.nextDouble();
                 m1 = (y1 - y2) / (x1 - x2);
                 yInt1 = y1 - m1 * x1;
             } else if (line1 == 3) {
-                System.out.print("slope= ");
+                System.out.print("slope=");
                 m1 = in.nextDouble();
-                System.out.print("y-intercept= ");
+                System.out.print("y-intercept=");
                 yInt1 = in.nextDouble();
             } else if (line1 == 4) {
-                System.out.print("x= ");
+                System.out.print("x=");
                 vert1 = in.nextDouble();
             }
             // Line 2
@@ -52,31 +52,31 @@ public class Lines {
             System.out.println("4) vertical line");
             int line2 = in.nextInt();
             if(line2 == 1) {
-                 System.out.print("slope= ");
+                 System.out.print("slope=");
                  m2 = in.nextDouble();
-                 System.out.print("x-coordinate= ");
+                 System.out.print("x-coordinate=");
                  double x = in.nextDouble();
-                 System.out.print("y-coordinate= ");
+                 System.out.print("y-coordinate=");
                  double y = in.nextDouble();
                  yInt2 = y - m2 * x;
             } else if (line2 == 2) {
-                System.out.print("x-coordinate of point 1= ");
+                System.out.print("x-coordinate of point 1=");
                 double x1 = in.nextDouble();
-                System.out.print("y-coordinate of point 2= ");
+                System.out.print("y-coordinate of point 2=");
                 double y1 = in.nextDouble();
-                System.out.print("x-coordinate= ");
+                System.out.print("x-coordinate=");
                 double x2 = in.nextDouble();
-                System.out.print("y-coordinate= ");
+                System.out.print("y-coordinate=");
                 double y2 = in.nextDouble();
                 m2 = (y1 - y2) / (x1 - x2);
                 yInt2 = y1 - m2 * x1;
             } else if (line2 == 3) {
-                System.out.print("slope= ");
+                System.out.print("slope=");
                 m2 = in.nextDouble();
-                System.out.print("y-intercept= ");
+                System.out.print("y-intercept=");
                 yInt2 = in.nextDouble();
             } else if  (line2 == 4) {
-                System.out.print("x= ");
+                System.out.print("x=");
                 vert2 = in.nextDouble();
             }
             //if one or both are vertical lines
@@ -88,9 +88,7 @@ public class Lines {
                 }
             } else if (line1 == 4 || line2 == 4) {
                 System.out.println("The lines intersect.");
-            }
-            //other three cases
-            if (m1 == m2) {
+            } else if (m1 == m2) { // other three cases
                 if (yInt1 == yInt2) { //slopes are equal and same y-intercept
                     System.out.println("The lines coincide.");
                 } else { //slopes are equal but not the same y-intercept
@@ -99,6 +97,7 @@ public class Lines {
             } else{ //different slope and different y-intercept
                 System.out.println("The lines intersect.");
             }
+            System.out.println("Enter '1' to rerun, or any other key and then enter to end.");
              if (in.hasNextInt() && in.nextInt() == 1) {
               continue;
             } else {
