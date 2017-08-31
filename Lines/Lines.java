5817e32c2a0319bc1c82552cca5d479d48c6eea3
Dustin Luong <dstn.luong@gmail.com>
2017-08-31 11:41:44 -0700

    reject Infinity case

--- Lines/Lines.java
+++ Lines/Lines.java
@@ -9,6 +9,9 @@ public class Lines {
             double yInt2 = 0;
             double vert1 = 0;
             double vert2 = 0;
+            double o = 1;
+            double p = 1;
+            double infinity = 1 / (o - p);
             // Line one
             System.out.println("Enter line 1:");
             System.out.println("1) point-slope");
@@ -27,14 +30,19 @@ public class Lines {
             } else if (line1 == 2){
                 System.out.print("x-coordinate of point 1=");
                 double x1 = in.nextDouble();
-                System.out.print("y-coordinate of point 2=");
+                System.out.print("y-coordinate of point 1=");
                 double y1 = in.nextDouble();
-                System.out.print("x-coordinate=");
+                System.out.print("x-coordinate of point 2=");
                 double x2 = in.nextDouble();
-                System.out.print("y-coordinate=");
+                System.out.print("y-coordinate of point 2=");
                 double y2 = in.nextDouble();
-                m1 = (y1 - y2) / (x1 - x2);
-                yInt1 = y1 - m1 * x1;
+                if (x1 == x2 && y1 == y2) {
+                    System.out.println("Enter two distinct points.");
+                    break;
+                } else {
+                    m1 = (y1 - y2) / (x1 - x2);
+                    yInt1 = y1 - m1 * x1;
+                }
             } else if (line1 == 3) {
                 System.out.print("slope=");
                 m1 = in.nextDouble();
@@ -62,12 +70,19 @@ public class Lines {
             } else if (line2 == 2) {
                 System.out.print("x-coordinate of point 1=");
                 double x1 = in.nextDouble();
-                System.out.print("y-coordinate of point 2=");
+                System.out.print("y-coordinate of point 1=");
                 double y1 = in.nextDouble();
-                System.out.print("x-coordinate=");
+                System.out.print("x-coordinate of point 2=");
                 double x2 = in.nextDouble();
-                System.out.print("y-coordinate=");
+                System.out.print("y-coordinate of point 2=");
                 double y2 = in.nextDouble();
+                if (x1 == x2 && y1 == y2) {
+                    System.out.println("Enter two distinct points.");
+                    break;
+                } else {
+                    m1 = (y1 - y2) / (x1 - x2);
+                    yInt1 = y1 - m1 * x1;
+                }
                 m2 = (y1 - y2) / (x1 - x2);
                 yInt2 = y1 - m2 * x1;
             } else if (line2 == 3) {
@@ -75,12 +90,15 @@ public class Lines {
                 m2 = in.nextDouble();
                 System.out.print("y-intercept=");
                 yInt2 = in.nextDouble();
-            } else if  (line2 == 4) {
+            } else if (line2 == 4) {
                 System.out.print("x=");
                 vert2 = in.nextDouble();
             }
-            //if one or both are vertical lines
-            if(line1 == 4 && line2 == 4) {
+            //
+            if (m1 == Math.abs(infinity) || m2 == Math.abs(infinity)) {
+                System.out.println("Please use input method 4 to input vertical lines.");
+                break;
+            } else if(line1 == 4 && line2 == 4) { //vertical lines cases
                 if (vert1 == vert2) {
                     System.out.println("The lines coincide.");
                 } else {
@@ -94,9 +112,13 @@ public class Lines {
                 } else { //slopes are equal but not the same y-intercept
                     System.out.println("The lines are parallel.");
                 }
-            } else{ //different slope and different y-intercept
+            } else { //different slope and different y-intercept
                 System.out.println("The lines intersect.");
             }
+            System.out.println(m1);
+            System.out.println(m2);
+            System.out.println(yInt1);
+            System.out.println(yInt2);
             System.out.println("Enter '1' to rerun, or any other key and then enter to end.");
              if (in.hasNextInt() && in.nextInt() == 1) {
               continue;
