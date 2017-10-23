c1c51319e6d285ed2456ebc62a117730d20d0f32
Dustin Luong <dstn.luong@gmail.com>
2017-10-23 12:09:21 -0700

    finished

--- RoachPopulation/RoachPopulationTester.java
+++ RoachPopulation/RoachPopulationTester.java
@@ -1,7 +1,7 @@
 public class RoachPopulationTester {
     public static void main (String[] args){
         RoachPopulation r = new RoachPopulation(100);
-        for(int i = 0; i < 3; i++){
+        for(int i = 0; i < 4; i++){
             r.breed();
             r.spray();
             System.out.println("Roach Population:" + r.getRoaches());
