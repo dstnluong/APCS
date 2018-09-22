7775d69ec10e5b85d68346c5d1bf38e933c2a45f
Dustin Luong <dstn.luong@gmail.com>
2018-09-22 11:10:12 -0700

    Update Factor.java

--- Factor/Factor.java
+++ Factor/Factor.java
@@ -1,7 +1,7 @@
 //Dustin Luong 10/02/17
 import static java.lang.Math.sqrt;
 import java.util.Scanner;
-public class Factor {
+public class Factoring {
     public static void main(String[] args) {
         while(true) {
             Scanner in = new Scanner(System.in);
@@ -10,14 +10,15 @@ public class Factor {
             double sqrt = sqrt(value);
             for(int i = 2; i <= sqrt; i++) {
                 if(value % i == 0){
-                    System.out.print(i + " ");
+                    System.out.printf("%d %n", i);
                     value /= i;
                     i--;
                 }
             }
-            //if(value != 1) System.out.println(value);
+            System.out.printf("%s%n", value == 1 ? "" : value);
             System.out.println("Enter '1' to rerun, or any other key and then enter to end.");
             if (in.hasNextInt() && in.nextInt() == 1) {
+                System.out.printf("%n");
                 continue;
             } else {
                 break;
